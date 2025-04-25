1f8f86d51f805486b54aaa07a94c5862a95e3f3c
Nemotte <jinzilong312@163.com>
2025-04-25 20:00:15 +0800

    first commit

---
+++ src/test/java/com/solorpgbackend/SolorpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solorpgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolorpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
