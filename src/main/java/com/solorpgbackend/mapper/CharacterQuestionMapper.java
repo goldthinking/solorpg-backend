2bb6e2062cb39634d82660dc015b7f29f7822a19
Nemotte <jinzilong312@163.com>
2025-04-25 20:12:37 +0800

    code generated

---
+++ src/main/java/com/solorpgbackend/mapper/CharacterQuestionMapper.java
@@ -0,0 +1,16 @@
+package com.solorpgbackend.mapper;
+
+import com.solorpgbackend.entity.CharacterQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Nemotte
+ * @since 2025-04-25
+ */
+public interface CharacterQuestionMapper extends BaseMapper<CharacterQuestion> {
+
+}
